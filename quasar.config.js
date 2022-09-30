32734540a9e84ac606eeaed77d0146cce3c4511a
rusfdev <rusfdev@icloud.com>
2022-09-30 22:54:41 +0300

    update publicPath

--- quasar.config.js
+++ quasar.config.js
@@ -62,7 +62,7 @@ module.exports = configure(function(ctx) {
         VERSION: version
       },
       // transpile: false,
-      // publicPath: '/',
+      publicPath: '/redlab-test/dist/spa/',
 
       // Add dependencies for transpiling with Babel (Array of string/regex)
       // (from node_modules, which are by default not transpiled).
